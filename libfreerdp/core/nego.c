a0b543b52201037049d1ff91d3d68f6e33bcbd21
U-TESTINO\Dmitriy <testino@outlook.com>
2015-02-08 16:07:23 +0200

    merge Fixed return type for nego_transport_connect and nego_transport_disco…

--- libfreerdp/core/nego.c
+++ libfreerdp/core/nego.c
@@ -62,8 +62,8 @@ static const char PROTOCOL_SECURITY_STRINGS[9][4] =
 };
 #endif /* WITH_DEBUG_NEGO */
 
-static int nego_transport_connect(rdpNego* nego);
-static int nego_transport_disconnect(rdpNego* nego);
+static BOOL nego_transport_connect(rdpNego* nego);
+static BOOL nego_transport_disconnect(rdpNego* nego);
 static BOOL nego_security_connect(rdpNego* nego);
 
 /**
@@ -278,7 +278,7 @@ BOOL nego_transport_connect(rdpNego* nego)
  * @return
  */
 
-int nego_transport_disconnect(rdpNego* nego)
+BOOL nego_transport_disconnect(rdpNego* nego)
 {
 	if (nego->tcp_connected)
 		transport_disconnect(nego->transport);
@@ -286,7 +286,7 @@ int nego_transport_disconnect(rdpNego* nego)
 	nego->tcp_connected = FALSE;
 	nego->security_connected = FALSE;
 
-	return 1;
+	return TRUE;
 }
 
 /**
